0125dc397071085b7bf6b0ebe101b56b04120dbb
JINO-ROHIT <find.jinorohit@gmail.com>
2025-06-23 20:57:12 +0530

    bash for setup

--- data_cleaning/scripts/setup.sh
+++ data_cleaning/scripts/setup.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 apt-get update
 
-apt-get install -y nano screen
+apt-get install -y screen nano
 
-apt-get install -y texlive-latex-base \
-                   texlive-fonts-recommended \
-                   texlive-fonts-extra \
-                   texlive-latex-extra
-
-apt-get install -y python3.12 python3.12-venv
-
-python3.12 -m venv venv
+python3 -m venv venv
 
 echo "Setup complete. To activate the virtual environment, run: source venv/bin/activate"
